6b7a4552f855e208fb0f94aa64cad557102a43aa
shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-23 22:40:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearBtn = document.getElementById("clear-cart-btn");
+
+
+// loading cart as empty
+let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
 // Render product list
 function renderProducts() {
@@ -19,19 +25,76 @@ function renderProducts() {
     li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
     productList.appendChild(li);
   });
+
+  const addBtn = document.querySelectorAll(".add-to-cart-btn");
+  addBtn.forEach(button => {
+    button.addEventListener("click", (event) => {
+      let productId = parseInt(event.target.getAttribute("data-id"));
+      addToCart(productId);
+    });
+  });
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+  cartList.innerHTML=""; // clear exxisting cart
+
+  if(cart.length === 0){
+    cartList.innerHTML = "Your cart is empty!"
+  } else {
+    cart.forEach((item,index) => {
+      const li = document.createElement("li");
+      li.innerHTML = `${item.name}-$${item.price} <button class="remove-from-cart-btn" data-id="${index}">Remove</button>`
+      cartList.appendChild(li);
+    });
+
+    const removeBtn = document.querySelectorAll(".remove-from-cart-btn");
+
+  removeBtn.forEach(button => {
+    button.addEventListener("click" , (e) => {
+      const itemIndex = parseInt(e.target.getAttribute("data-id"));
+      removeFromCart(itemIndex);
+    });
+  });
+
+    
+  }
+
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+  const product = products.find((product) => product.id === productId);
+  
+  if(product){
+    // we will check product is already present in cart or not?
+    const cartItemIndex = cart.findIndex((item) => item.id === product.id);
+
+    if(cartItemIndex === -1){
+      cart.push(product);
+    }
+
+    sessionStorage.setItem("cart",JSON.stringify(cart));
+
+    renderCart();
+  }
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(itemIndex) {
+  cart.splice(itemIndex,1);
+  sessionStorage.setItem("cart",JSON.stringify(cart));
+  renderCart();
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+  cart = [];
+  sessionStorage.removeItem("cart");
+  renderCart();
+}
+
+clearBtn.addEventListener("click", clearCart);
 
 // Initial render
 renderProducts();
